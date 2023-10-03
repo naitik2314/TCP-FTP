cc8e423909f3a1a7d56df3624f1e1ee59d6b973a
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2023-10-03 16:21:54 -0400

    New commit

--- client.py
+++ client.py
@@ -1,1 +1,79 @@
+import socket #To send the message accross internet
+import hashlib #To Check MD5
 
+#Function to connect to the server
+def connect_to_server(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    return client_socket
+
+#Functio to send a command to the server
+def send_command(client_socket, command):
+    client_socket.send(command_encode())
+
+#Function to receive data from the server
+def receive_data(client_socket):
+    data = b""
+    while True:
+        chunk = client_socket.recv(1024)
+        if not chunk:
+            break
+        data += chunk
+    return data.decode()
+
+#Function to verify MD5 checksum of the downloaded file
+def verify_md5(file_path, received_md5):
+    md5_hash = hashlib.md5(open(file_path, "rb").read()).hexdigest()
+    return md5_hash == received_md5
+
+#Main Function
+def main():
+    server_ip = "127.0.0.1" 
+    server_port = 12345
+
+    try:
+        client_socket = connect_to_server(server_ip, server_port)
+        while True:
+            print("Available Commands:")
+            print("1. LIST - list files")
+            print("2. GET <file_name> - Download file")
+            print("3. CLOSE - Close connection")
+
+            user_input = input("Enter command: ").strip()
+            send_command(client_socket=client_socket, user_input=user_input)
+
+            if user_input == "CLOSE":
+                break
+            elif user_input == "LIST":
+                data = receive_data(client_socket)
+                print(data)
+            elif user_input.startswith("GET "):
+                _, file_name, start_byte, end_byte = user_input.split()
+                start_byte = int(start_byte)
+                end_byte = int(end_byte)
+                file_data = receive_data(client_socket)
+                
+                if file_data.startswith("<REP GET BEGIN>"):
+                    file_data = file_data[len("<REP GET BEGIN>\n"):]
+                    
+                    if end_byte == -1:
+                        file_path = f"downloaded_{file_name}"
+                    else:
+                        file_path = f"downloaded_{file_name}_{start_byte}_{end_byte}"
+                    
+                    with open(file_path, "wb") as file:
+                        file.write(file_data.encode())
+                    
+                    received_md5 = receive_data(client_socket)
+                    
+                    if verify_md5(file_path, received_md5):
+                        print(f"File {file_name} downloaded successfully.")
+                    else:
+                        print(f"File {file_name} download failed (checksum mismatch).")
+                else:
+                    print(file_data)
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,1 +1,78 @@
+import socket
+import os
+import hashlib
+import time
+
+# Read configuration parameters from a configuration file
+def read_config():
+    config = {}
+    with open("server_config.txt", "r") as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            config[key.strip()] = value.strip()
+    return config
+
+# Function to send the file list to the client
+def send_file_list(conn, shared_dir):
+    file_list = os.listdir(shared_dir)
+    response = f"<REP LIST {len(file_list)}>\n"
+    for idx, file_name in enumerate(file_list, start=1):
+        file_path = os.path.join(shared_dir, file_name)
+        file_size = os.path.getsize(file_path)
+        md5_hash = hashlib.md5(open(file_path, "rb").read()).hexdigest()
+        response += f"<{idx} {file_name} {file_size} {md5_hash}>\n"
+    response += "<REP LIST END>\n"
+    conn.send(response.encode())
+
+# Function to send a portion of a file to the client
+def send_file_chunk(conn, file_path, start_byte, end_byte):
+    with open(file_path, "rb") as file:
+        file.seek(start_byte)
+        data = file.read(end_byte - start_byte)
+        conn.send(data)
+
+# Function to handle client requests
+def handle_client(conn, addr, shared_dir):
+    print(f"Accepted connection from {addr[0]}:{addr[1]}")
+    conn.settimeout(120)  # Timeout if idle for 2 minutes
+    try:
+        while True:
+            command = conn.recv(1024).decode().strip()
+            if not command:
+                break
+            if command == "<REQ LIST>":
+                send_file_list(conn, shared_dir)
+            elif command.startswith("<GET "):
+                _, file_name, start_byte, end_byte = command.split()
+                start_byte = int(start_byte)
+                end_byte = int(end_byte)
+                file_path = os.path.join(shared_dir, file_name)
+                send_file_chunk(conn, file_path, start_byte, end_byte)
+            elif command == "<CLOSE>":
+                break
+    except socket.timeout:
+        print(f"Connection from {addr[0]}:{addr[1]} timed out.")
+    finally:
+        print(f"Closed connection from {addr[0]}:{addr[1]}")
+        conn.close()
+
+# Main function to start the server
+def main():
+    config = read_config()
+    host = config["server_ip"]
+    port = int(config["server_port"])
+    shared_dir = config["shared_directory"]
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)  # Listen for incoming connections
+
+    print(f"Server listening on {host}:{port}")
+
+    while True:
+        conn, addr = server_socket.accept()
+        handle_client(conn, addr, shared_dir)
+
+if __name__ == "__main__":
+    main()
 
